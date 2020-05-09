64da24a4a06881b30ab9622e3dfea2155b683300
nivabecassis <abecassis.niv@gmail.com>
2020-05-09 10:46:12 -0400

    Add express router

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Get routes
+const bootcamps = require("./routes/bootcamps");
+
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Get all bootcamps" });
-});
-
-app.get("/api/v1/bootcamps:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(201).json({ success: true, msg: "Create new bootcamp" });
-});
-
-app.put("/api/v1/bootcamps:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
