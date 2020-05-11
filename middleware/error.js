350504f62fd925d5cb4181b9f67bbacb54b67761
nivabecassis <abecassis.niv@gmail.com>
2020-05-10 22:38:14 -0400

    Add some more error processing

--- middleware/error.js
+++ middleware/error.js
@@ -1,13 +1,28 @@
 const ApiError = require("../utils/ApiError");
 
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+  console.log(err);
 
   let error = { ...err };
 
   error.message = err.message || "Server error";
+
+  // Mongoose Cast error
   if (error.name === "CastError") {
     error = new ApiError(`Resource not found with ID ${error.value}`, 404);
+  }
+
+  // Mongoose duplicate key error
+  if (error.code === 11000) {
+    error = new ApiError("Duplicate field value entered", 400);
+  }
+
+  // Mongoose validation error
+  if (error.name === "ValidationError") {
+    error = new ApiError(
+      Object.values(error.errors).map(e => e.message),
+      400
+    );
   }
 
   res
