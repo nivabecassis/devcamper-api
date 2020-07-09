39805aa6b136558ea77afedfb2175d55833f409c
nivabecassis <abecassis.niv@gmail.com>
2020-07-08 23:21:27 -0400

    Generate token for registering user

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,12 +8,14 @@ const User = require("../models/User");
 exports.registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
-  const createdUser = await User.create({
+  const user = await User.create({
     name,
     email,
     password,
     role,
   });
 
-  res.status(200).json({ success: true });
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
