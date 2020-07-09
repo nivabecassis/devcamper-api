3ed1d58acceaf2f9ead41ef0bf67e200eef9bc2f
nivabecassis <abecassis.niv@gmail.com>
2020-07-08 23:41:47 -0400

    Implement user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,28 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ApiError("Missing credentials", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ApiError("Invalid credentials", 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ApiError("Invalid credentials", 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
