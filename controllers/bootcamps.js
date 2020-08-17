aafbc12bd98139074c29e2a73fd7e588b1d75ac9
nivabecassis <abecassis.niv@gmail.com>
2020-08-17 16:46:27 -0400

    Add publisher bootcamp limit

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,6 +29,23 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Save the user that created the bootcamp
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // Publishers can only create one bootcamp
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ApiError(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
+  // Create the bootcamp
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({ success: true, data: bootcamp });
 });
