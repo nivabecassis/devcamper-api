74afb844272eedc883289de475838c8d9802504f
nivabecassis <abecassis.niv@gmail.com>
2020-08-23 22:13:23 -0400

    Add CRUD functionality for users

---
+++ controllers/users.js
@@ -0,0 +1,64 @@
+const ApiError = require("../utils/ApiError");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
+
+// @desc    Get all users
+// @route   GET /api/v1/auth/users
+// @access  Private/Admins
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: res.advancedResults,
+  });
+});
+
+// @desc    Get a user
+// @route   GET /api/v1/auth/users/:id
+// @access  Private/Admins
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Create user
+// @route   POST /api/v1/auth/users
+// @access  Private/Admins
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Update user
+// @route   PUT /api/v1/auth/users/:id
+// @access  Private/Admins
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Delete user
+// @route   DELETE /api/v1/auth/users/:id
+// @access  Private/Admins
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
